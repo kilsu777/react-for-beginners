b45535f2884ccbc9243a8cfe8fb98099688e18be
kilsu777 <kilsu777@yahoo.co.kr>
2024-07-17 17:46:50 +0900

    4th commit

--- src/App6.js
+++ src/App6.js
@@ -7,12 +7,14 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
+  const URL = process.env.PUBLIC_URL;
+  console.log(URL);
   return (
     <Router>
       <Routes>
-        <Route path="/hello" element={<h1>Hello</h1>} />
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path={URL + "/hello"} element={<h1>Hello</h1>} />
+        <Route path={URL + "/movie/:id"} element={<Detail />} />
+        <Route path={URL + "/"} element={<Home />} />
       </Routes>
     </Router>
   );

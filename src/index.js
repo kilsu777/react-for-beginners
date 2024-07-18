b3744a66f88e9e1fadfeab0e1fd306eaf4812794
kilsu777 <kilsu777@yahoo.co.kr>
2024-07-18 12:23:08 +0900

    7th commit

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App6';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 // import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
-  // </React.StrictMode>
-  // <App />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 console.log("Done");
+
+// <BrowserRouter basename={process.env.PUBLIC_URL}>
+//   <App />
+// </BrowserRouter>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

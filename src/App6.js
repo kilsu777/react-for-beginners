597df357b824213eaf46a2eeec5507a23edb66da
kilsu777 <kilsu777@yahoo.co.kr>
2024-07-18 12:00:00 +0900

    6th commit

--- src/App6.js
+++ src/App6.js
@@ -7,14 +7,14 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
-  const URL = process.env.PUBLIC_URL;
+  // const URL = process.env.PUBLIC_URL;
   console.log(URL);
   return (
     <Router>
       <Routes>
         <Route path="/hello" element={<h1>Hello</h1>} />
         <Route path="/movie/:id" element={<Detail />} />
-        <Route path={URL + "/"} element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App6';
+import { BrowserRouter } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 // import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+  // </React.StrictMode>
+  // <App />
+  // </React.StrictMode>
 );
 
 console.log("Done");
